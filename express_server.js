2ea979f8d76e186e286f2d5d08369bc9f04329df
Simon Garber <Simon.garber@gmail.com>
2019-07-08 18:33:00 -0400

    Completed and tested the get route for shortURL. The link for the long url to the corresponding shortURL is being rendered in the browser and the route works.

--- express_server.js
+++ express_server.js
@@ -3,8 +3,9 @@ const app = express();
 const PORT = 8080;
 
 app.set("view engine", "ejs");
+
 const urlDatabase = {
-    "bZVn2": "http://www.lighthouselabs.ca",
+    "b2xVn2": "http://www.lighthouselabs.ca",
     "9sm5xK": "http://google.com"
 };
 
@@ -18,9 +19,26 @@ app.get("/urls.json", (req,res) => {
 })
 
 app.get("/hello", (req,res) => {
-    res.send("<html><body>hello <b>World</b></body</html>\n");
+    let templateVars = {greeting: "Hello World!"};
+    res.render("Hello_World", templateVars);
+});
+
+app.get("/urls", (req,res) => {
+   
+    let templateVars = { urls: urlDatabase};
+    res.render("urls_index", templateVars);
+    
+});
+
+app.get("/urls/:shortURL", (req,res) => {
+console.log(req.params.shortURL)
+let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+
+res.render("urls_show", templateVars);
+
 });
 
 app.listen(PORT, () => {
+    
     console.log(`Example app listening on port ${PORT}!`);
 });
