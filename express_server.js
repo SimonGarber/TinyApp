6c2ee555970ca80d5d729d26fb58d9730f76d20a
Simon Garber <Simon.garber@gmail.com>
2019-07-08 20:31:52 -0400

    implemented the u/shortURL functionality that links directly to the corresponding longurl both in the terminal and in the browser.

--- express_server.js
+++ express_server.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
-
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+const generateRandomString = function () {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let charactersLength = characters.length;
+    for (let i = 0; i < 6; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+  
+    return result;
+  };  
 
 const urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
@@ -30,13 +42,35 @@ app.get("/urls", (req,res) => {
     
 });
 
+app.get("/urls/new", (req,res) => {
+    res.render("urls_new");  
+})
+
 app.get("/urls/:shortURL", (req,res) => {
-console.log(req.params.shortURL)
+// console.log(req.params.shortURL)
 let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
 
 res.render("urls_show", templateVars);
 
 });
+
+app.get("/u/:shortURL", (req,res) => {
+    const longURL = urlDatabase[req.params.shortURL]
+    
+    res.redirect(longURL)
+});
+
+app.post("/urls", (req, res) => {
+    let shortURL = generateRandomString();
+    urlDatabase[shortURL] =  req.body.longURL
+    console.log(urlDatabase[shortURL])
+    res.redirect(`/urls/${shortURL}`);
+    
+});
+
+
+
+
 
 app.listen(PORT, () => {
     
